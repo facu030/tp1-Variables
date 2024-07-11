5f87bb2b472628f272a5b977dff60f4b860914f4
Facundo Martinez Rodriguez <facundomartinezr03@gmail.com>
2024-07-11 00:24:13 -0300

    no terminado ejer 9

---
+++ js/Ejercicio-09.js
@@ -0,0 +1,11 @@
+/*
+ Escribe un programa que pida una frase y escriba las vocales que aparecen
+
+Ejemplo:
+input: Hola mundo
+Output: oauo
+*/
+
+let frase = new Array(prompt("ingrese una frase"));
+console.log(frase.length[0])
+var vocal_a =0, vocal_e =0, vocal_i =0, vocal_o =0 ,vocal_u =0;     
